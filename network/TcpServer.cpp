043de169b4f915a631c4b330e0699ecc9676768e
Ed Asriyan <asriyan1998@yandex.ru>
2016-11-30 02:30:57 +0300

    Implement the TcpServer

--- network/TcpServer.cpp
+++ network/TcpServer.cpp
@@ -10,6 +10,43 @@ Network::TcpServer::TcpServer(unsigned short port) : port(port) {
 	}
 }
 
+void Network::TcpServer::start() {
+	boost::thread(listen);
+}
+
+void Network::TcpServer::stop() {
+	running = false;
+}
+
+void Network::TcpServer::listen() {
+	if (running) {
+		return;
+	}
+
+	running = true;
+
+	ip::tcp::acceptor acceptor(service, ip::tcp::endpoint(ip::tcp::v4(), port));
+
+	while (running) {
+		socket_ptr socket(new ip::tcp::socket(service));
+		acceptor.accept(*socket);
+		boost::thread(boost::bind(&TcpServer::handleConnection, this, socket));
+	}
+
+}
+
 unsigned short Network::TcpServer::get_port() const {
 	return port;
+}
+
+bool Network::TcpServer::is_running() const {
+	return running;
+}
+
+void Network::TcpServer::handleConnection(socket_ptr socket) {
+	try {
+		OnConnect(socket);
+	} catch (...) {
+
+	}
 }
--- network/TcpServer.h
+++ network/TcpServer.h
@@ -8,8 +8,11 @@
 #include <stdexcept>
 
 #include <boost/shared_ptr.hpp>
-#include <boost/signals2/signal.hpp>
+#include <boost/thread.hpp>
+#include <boost/signals2.hpp>
 #include <boost/asio.hpp>
+
+#include "Connection.h"
 
 namespace Network {
 	using namespace boost::asio;
@@ -22,6 +25,9 @@ namespace Network {
 	class TcpServer {
 		private:
 			unsigned short port;
+			io_service service;
+
+			bool running = false;
 
 		public:
 			/**
@@ -36,10 +42,34 @@ namespace Network {
 			TcpServer(unsigned short port);
 
 			/**
+			 * @brief Starts the server.
+			 */
+			void start();
+
+			/**
+			 * @brief Stops the server.
+			 */
+			void stop();
+
+			/**
+			 * @brief Run synchronous listening in current thread.
+			 */
+			void listen();
+
+			/**
 			 * @brief Returns listening port.
 			 * @return Listening port.
 			 */
 			unsigned short get_port() const;
+
+			/**
+			 * @brief Returns true if server is running.
+			 * @return True if server is running.
+			 */
+			bool is_running() const;
+
+		private:
+			void handleConnection(socket_ptr socket);
 
 	};
 }
